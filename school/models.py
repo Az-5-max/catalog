a245b522d4e31bf4476470b9abf9a2bde9261dfd
Azaliya <azaliyazagitovna@gmail.com>
2026-07-09 22:44:24 +0300

    добавила связь многие ко многим

---
+++ school/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    # СВЯЗЬ МНОГИЕ КО МНОГИМ (правильно!)
+    teachers = models.ManyToManyField(Teacher, related_name='students')
+
+    def __str__(self):
+        return self.name
